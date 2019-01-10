94a70c76b96428779468887dea828424998e260f
Sam Belliveau <sam.belliveau@gmail.com>
2019-01-10 17:57:06 -0500

    Setup robot in new structure.

--- LimelightBot/src/main/java/frc/robot/Robot.java
+++ LimelightBot/src/main/java/frc/robot/Robot.java
@@ -8,90 +8,139 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.util.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
+ * creating this project, you must also update the build.properties file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+	public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+	public static OI m_oi;
+	public NetworkTable table;
+	//NetworkTableClient client;
+	Command m_autonomousCommand;
+	SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		m_oi = new OI();
+		table = NetworkTableInstance.getDefault().getTable("limelight");
+		m_chooser.addDefault("Default Auto", new ExampleCommand());
+		// chooser.addObject("My Auto", new MyAutoCommand());
+		SmartDashboard.putData("Auto mode", m_chooser);
+		//client = new NetworkTableClient("limelight");
+	}
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	@Override
+	public void disabledInit() {
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
+	}
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-  }
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString code to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
+	 */
+	@Override
+	public void autonomousInit() {
+		m_autonomousCommand = m_chooser.getSelected();
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+		/*
+		 * String autoSelected = SmartDashboard.getString("Auto Selector",
+		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+		 * = new MyAutoCommand(); break; case "Default Auto": default:
+		 * autonomousCommand = new ExampleCommand(); break; }
+		 */
+
+		// schedule the autonomous command (example)
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.start();
+		}
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.cancel();
+		}
+	}
+
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+
+		// double x = LimeLight.getXOffset();
+		// double y = LimeLight.getYOffset();
+		// double area = LimeLight.getTargetArea();
+
+		NetworkTableEntry tx = table.getEntry("tx");
+		NetworkTableEntry ty = table.getEntry("ty");
+		NetworkTableEntry ta = table.getEntry("ta");
+		
+		//read values periodically
+		double x = tx.getDouble(0.0);
+		double y = ty.getDouble(0.0);
+		double area = ta.getDouble(0.0);
+
+		//post to smart dashboard periodically
+		SmartDashboard.putNumber("LimelightX", x);
+		SmartDashboard.putNumber("LimelightY", y);
+		SmartDashboard.putNumber("LimelightArea", area);
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
 }
---
+++ LimelightBot/src/main/java/frc/robot/util/LimeLight.java
@@ -0,0 +1,178 @@
+/* Lime Light Docs: http://docs.limelightvision.io/en/latest/networktables_api.html# */
+
+package frc.robot.util;
+
+import frc.robot.util.NetworkTableClient;
+
+// Using enums because random constants are unreadable
+public enum LED_MODE {
+  PIPELINE(0),    // Use LED mode set in pipeline 
+  FORCE_OFF(1),   // Force LEDs off
+  FORCE_BLINK(2), // Force LEDs to blink
+  FORCE_ON(3);    // Force LEDs on 
+
+  LED_MODE(int value) { this.val = value; }
+  public int getCodeValue() { return val; }
+  private int val;
+};
+
+public enum CAM_MODE {
+  VISION(0), // Use limelight for CV
+  DRIVER(1); // Use limelight for driving (this is dumb, dont do this)
+
+  LED_MODE(int value) { this.val = value; }
+  public int getCodeValue() { return val; }
+  private int val;
+};
+
+public enum STREAM { // PIP = Picture-In-Picture
+  STANDARD(0), PIP_MAIN(1), PIP_SECONDARY(2);
+
+  LED_MODE(int value) { this.val = value; }
+  public int getCodeValue() { return val; }
+  private int val;
+};
+
+public enum SNAPSHOT_MODE {
+  STOP(0), // Don't take snapshots
+  TAKE_TWO_PER_SECOND(1); // Take two snapshots per second
+  
+  LED_MODE(int value) { this.val = value; }
+  public int getCodeValue() { return val; }
+  private int val;
+};
+
+class LimeLight {
+  // Network Table used to contact Lime Light
+  private static NetworkTableClient table = new NetworkTableClient("limelight");
+
+  /* “Best” Contour information */
+  // Whether the limelight has any valid targets (0 or 1)
+  public static boolean hasValidTarget() {
+    // == 1 converts double to boolean
+    return table.getDouble("tv") == 1;
+  }
+
+  // Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
+  public static final double MIN_X_OFFSET = -27;
+  public static final double MAX_X_OFFSET = 27;
+  public static double getTargetXOffset() {
+    return table.getDouble("tx");
+  }
+
+  // Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
+  public static final double MIN_Y_OFFSET = -20.5;
+  public static final double MAX_Y_OFFSET = 20.5;
+  public static double getTargetYOffset() {
+    return table.getDouble("ty");
+  }
+
+  // Target Area (0% of image to 100% of image)
+  public static final double MIN_TARGET_AREA = 0;
+  public static final double MAX_TARGET_AREA = 1;
+  public static double getTargetArea() {
+    // Lime light returns a double from 0 - 100
+    // Divide by 100 to scale number from 0 - 1
+    return table.getDouble("ta") / 100.0;
+  }
+
+  // Skew or rotation (-90 degrees to 0 degrees)
+  public static final double MIN_SKEW = -90;
+  public static final double MAX_SKEW = 0;
+  public static double getSkew() {
+    return table.getDouble("ts");
+  }
+
+  // The pipeline’s latency contribution (ms) Add at
+  // least 11ms for image capture latency.
+  public static final double IMAGE_CAPTURE_LATENCY = 11;
+  public static double getLatency() {
+    // Add Image Capture Latency to 
+    // get more accurate result
+    return table.getDouble("tl") + IMAGE_CAPTURE_LATENCY;
+  }
+
+  /* Camera Controls (Use Enums to prevent invalid inputs) */
+  // ledMode  |	Sets limelight’s LED state
+  // 0        |	use the LED Mode set in the current pipeline
+  // 1        |	force off
+  // 2        |	force blink
+  // 3        |	force on
+  public static void setLEDMode(LED_MODE mode) {
+    table.setNumber("ledMode", mode.getCodeValue());
+  }
+
+  // camMode  | Sets limelight’s operation mode
+  // ---------+--------------------------------
+  // 0        |	Vision processor
+  // 1        |	Driver Camera (Increases exposure, disables vision processing)
+  public static void setCamMode(CAM_MODE mode) {
+    table.setNumber("camMode", mode.getValueCode());
+  }
+
+  // pipeline |	Sets limelight’s current pipeline
+  // ---------+----------------------------------
+  // 0..9     |	Select pipeline 0..9
+  public static void setPipeline(int pipeline) {
+    // Prevent input of invalid pipelines
+    if(pipeline >= 0 && pipeline <= 9) { 
+      table.setNumber("pipeline", pipeline);
+    }
+  }
+
+  // stream   |	Sets limelight’s streaming mode
+  // ---------+--------------------------------
+  // 0        |	Standard - Side-by-side streams if a webcam is attached to Limelight
+  // 1        |	PiP Main - The secondary camera stream is placed in the lower-right corner of the primary camera stream
+  // 2        |	PiP Secondary - The primary camera stream is placed in the lower-right corner of the secondary camera stream
+  public static void setStream(STREAM stream) {
+    table.setNumber("stream", stream.getCodeValue());
+  }
+
+  // snapshot |	Allows users to take snapshots during a match
+  // ---------+----------------------------------------------
+  // 0        |	Stop taking snapshots
+  // 1        |	Take two snapshots per second
+  public static void setSnapshotMode(SNAPSHOT_MODE snapshot) {
+    table.setNumber("snapshot", snapshot.getCodeValue());
+  }
+
+  /* Advanced Usage with Raw Contours */
+  /* Raw Targets */
+
+  // Raw Contours are formatted as tx0, ty0, tx1, ty1, tx2, ty2
+  // So to make this easier, you pass an int and it formats it
+
+  // Raw Screenspace X
+  public static double getTX(int Target) {
+    return table.getDouble("tx" + Integer.toString(Target));
+  }
+
+  // Raw Screenspace Y
+  public static double getTY(int Target) {
+    return table.getDouble("ty" + Integer.toString(Target));
+  }
+
+  // Area (0% of image to 100% of image)
+  public static double getTA(int Target) {
+    // Lime light returns a double from 0 - 100
+    // Divide by 100 to scale number from 0 - 1
+    return table.getDouble("ta" + Integer.toString(Target)) / 100.0;
+  }
+
+  // Skew or rotation (-90 degrees to 0 degrees)
+  public static double getTS(int Target) {
+    return table.getDouble("ts" + Integer.toString(Target));
+  }
+
+  /* Raw Crosshairs */
+  // Crosshair A X in normalized screen space
+  public static double getCX(int crosshair) {
+    return table.getDouble("cx" + Integer.toString(crosshair));
+  }
+
+  // Crosshair A Y in normalized screen space
+  public static double getCY(int crosshair) {
+    return table.getDouble("cy" + Integer.toString(crosshair));
+  }
+}
