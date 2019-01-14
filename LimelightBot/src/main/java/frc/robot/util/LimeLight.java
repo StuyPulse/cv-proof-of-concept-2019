f56d0116c5957494ce1217d265cfcbb24699f483
Sam Belliveau <sam.belliveau@gmail.com>
2019-01-14 16:12:48 -0500

    Moved around Limelight.java

--- LimelightBot/src/main/java/frc/robot/util/LimeLight.java
+++ LimelightBot/src/main/java/frc/robot/util/LimeLight.java
@@ -5,44 +5,6 @@ package frc.robot.util;
 import frc.robot.util.NetworkTableClient;
 
 public class LimeLight {
-  // Using enums because random constants are unreadable
-  public enum LED_MODE {
-    PIPELINE(0),    // Use LED mode set in pipeline 
-    FORCE_OFF(1),   // Force LEDs off
-    FORCE_BLINK(2), // Force LEDs to blink
-    FORCE_ON(3);    // Force LEDs on 
-
-    LED_MODE(int value) { this.val = value; }
-    public int getCodeValue() { return val; }
-    private int val;
-  };
-
-  public enum CAM_MODE {
-    VISION(0), // Use limelight for CV
-    DRIVER(1); // Use limelight for driving (this is dumb, dont do this)
-
-    CAM_MODE(int value) { this.val = value; }
-    public int getCodeValue() { return val; }
-    private int val;
-  };
-
-  public enum STREAM { // PIP = Picture-In-Picture
-    STANDARD(0), PIP_MAIN(1), PIP_SECONDARY(2);
-
-    STREAM(int value) { this.val = value; }
-    public int getCodeValue() { return val; }
-    private int val;
-  };
-
-  public enum SNAPSHOT_MODE {
-    STOP(0), // Don't take snapshots
-    TAKE_TWO_PER_SECOND(1); // Take two snapshots per second
-    
-    SNAPSHOT_MODE(int value) { this.val = value; }
-    public int getCodeValue() { return val; }
-    private int val;
-  };
-
   // Network Table used to contact Lime Light
   private static NetworkTableClient table = new NetworkTableClient("limelight");
 
@@ -123,26 +85,37 @@ public class LimeLight {
   }
 
   /* Camera Controls (Use Enums to prevent invalid inputs) */
-  // ledMode  |	Sets limelight’s LED state
-  // 0        |	use the LED Mode set in the current pipeline
-  // 1        |	force off
-  // 2        |	force blink
-  // 3        |	force on
+  // LED_MODE
+  public enum LED_MODE {
+    PIPELINE(0),    // Use LED mode set in pipeline 
+    FORCE_OFF(1),   // Force LEDs off
+    FORCE_BLINK(2), // Force LEDs to blink
+    FORCE_ON(3);    // Force LEDs on 
+
+    LED_MODE(int value) { this.val = value; }
+    public int getCodeValue() { return val; }
+    private int val;
+  };
+
   public static void setLEDMode(LED_MODE mode) {
     table.setNumber("ledMode", mode.getCodeValue());
   }
 
-  // camMode  | Sets limelight’s operation mode
-  // ---------+--------------------------------
-  // 0        |	Vision processor
-  // 1        |	Driver Camera (Increases exposure, disables vision processing)
+  // CAM_MODE
+  public enum CAM_MODE {
+    VISION(0), // Use limelight for CV
+    DRIVER(1); // Use limelight for driving (this is dumb, dont do this)
+
+    CAM_MODE(int value) { this.val = value; }
+    public int getCodeValue() { return val; }
+    private int val;
+  };
+
   public static void setCamMode(CAM_MODE mode) {
     table.setNumber("camMode", mode.getCodeValue());
   }
 
-  // pipeline |	Sets limelight’s current pipeline
-  // ---------+----------------------------------
-  // 0..9     |	Select pipeline 0..9
+  // PIPELINE
   public static void setPipeline(int pipeline) {
     // Prevent input of invalid pipelines
     if(pipeline >= 0 && pipeline <= 9) { 
@@ -150,19 +123,29 @@ public class LimeLight {
     }
   }
 
-  // stream   |	Sets limelight’s streaming mode
-  // ---------+--------------------------------
-  // 0        |	Standard - Side-by-side streams if a webcam is attached to Limelight
-  // 1        |	PiP Main - The secondary camera stream is placed in the lower-right corner of the primary camera stream
-  // 2        |	PiP Secondary - The primary camera stream is placed in the lower-right corner of the secondary camera stream
+  // STREAM
+  public enum STREAM { // PIP = Picture-In-Picture
+    STANDARD(0), PIP_MAIN(1), PIP_SECONDARY(2);
+
+    STREAM(int value) { this.val = value; }
+    public int getCodeValue() { return val; }
+    private int val;
+  };
+
   public static void setStream(STREAM stream) {
     table.setNumber("stream", stream.getCodeValue());
   }
 
-  // snapshot |	Allows users to take snapshots during a match
-  // ---------+----------------------------------------------
-  // 0        |	Stop taking snapshots
-  // 1        |	Take two snapshots per second
+  // SNAPSHOT_MODE
+  public enum SNAPSHOT_MODE {
+    STOP(0), // Don't take snapshots
+    TAKE_TWO_PER_SECOND(1); // Take two snapshots per second
+    
+    SNAPSHOT_MODE(int value) { this.val = value; }
+    public int getCodeValue() { return val; }
+    private int val;
+  };
+
   public static void setSnapshotMode(SNAPSHOT_MODE snapshot) {
     table.setNumber("snapshot", snapshot.getCodeValue());
   }
