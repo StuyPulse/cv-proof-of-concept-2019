2c336da9946b27edebdbc5b71b9055883ed1e8ef
Sam Belliveau <sam.belliveau@gmail.com>
2019-01-21 15:40:26 -0500

    Fixed spelling and Formatting

--- LimelightBot/src/main/java/frc/util/LimeLight.java
+++ LimelightBot/src/main/java/frc/util/LimeLight.java
@@ -19,6 +19,7 @@ public class LimeLight {
   // Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
   public static final double MIN_X_OFFSET = -27;
   public static final double MAX_X_OFFSET = 27;
+
   public static double getTargetXOffset() {
     return table.getDouble("tx");
   }
@@ -26,18 +27,19 @@ public class LimeLight {
   // Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
   public static final double MIN_Y_OFFSET = -20.5;
   public static final double MAX_Y_OFFSET = 20.5;
+
   public static double getTargetYOffset() {
     return table.getDouble("ty");
   }
-  
+
   // Calculate Distance using TY
   public static double getTargetDistance(double HeightFromCamera) {
     return HeightFromCamera / Math.tan(Math.toRadians(getTargetYOffset()));
   }
 
-  // Calculate Cordinates using Distance as the radius of a circle
+  // Calculate Coordinates using Distance as the radius of a circle
   // and using TX to get which point of the circle it is
-  public static Vector2d getTargetCordinates(double HeightFromCamera) {
+  public static Vector2d getTargetCoordinates(double HeightFromCamera) {
     final double DISTANCE = getTargetDistance(HeightFromCamera);
     final double XOFFSET = Math.toRadians(getTargetXOffset());
     return new Vector2d(DISTANCE * Math.sin(XOFFSET), DISTANCE * Math.cos(XOFFSET));
@@ -46,6 +48,7 @@ public class LimeLight {
   // Target Area (0% of image to 100% of image)
   public static final double MIN_TARGET_AREA = 0;
   public static final double MAX_TARGET_AREA = 1;
+
   public static double getTargetArea() {
     // Lime light returns a double from 0 - 100
     // Divide by 100 to scale number from 0 - 1
@@ -55,6 +58,7 @@ public class LimeLight {
   // Skew or rotation (-90 degrees to 0 degrees)
   public static final double MIN_SKEW = -90;
   public static final double MAX_SKEW = 0;
+
   public static double getTargetSkew() {
     return table.getDouble("ts");
   }
@@ -62,8 +66,9 @@ public class LimeLight {
   // The pipeline’s latency contribution (ms) Add at
   // least 11ms for image capture latency.
   public static final double IMAGE_CAPTURE_LATENCY = 11;
+
   public static double getLatency() {
-    // Add Image Capture Latency to 
+    // Add Image Capture Latency to
     // get more accurate result
     return table.getDouble("tl") + IMAGE_CAPTURE_LATENCY;
   }
@@ -81,6 +86,7 @@ public class LimeLight {
   // Horizontal sidelength of the rough bounding box (0 - 320 pixels)
   public static final double MIN_HORIZ_SIDE_LENGTH = 0;
   public static final double MAX_HORIZ_SIDE_LENGTH = 320;
+
   public static double getHorizontalSidelength() {
     return table.getDouble("thoriz");
   }
@@ -88,6 +94,7 @@ public class LimeLight {
   // Vertical sidelength of the rough bounding box (0 - 320 pixels)
   public static final double MIN_VIRT_SIDE_LENGTH = 0;
   public static final double MAX_VIRT_SIDE_LENGTH = 320;
+
   public static double getVirticalSidelength() {
     return table.getDouble("tvert");
   }
@@ -105,13 +112,19 @@ public class LimeLight {
   /* Camera Controls (Use Enums to prevent invalid inputs) */
   // LED_MODE
   public enum LED_MODE {
-    PIPELINE(0),    // Use LED mode set in pipeline 
-    FORCE_OFF(1),   // Force LEDs off
+    PIPELINE(0), // Use LED mode set in pipeline
+    FORCE_OFF(1), // Force LEDs off
     FORCE_BLINK(2), // Force LEDs to blink
-    FORCE_ON(3);    // Force LEDs on 
+    FORCE_ON(3); // Force LEDs on
 
-    LED_MODE(int value) { this.val = value; }
-    public int getCodeValue() { return val; }
+    LED_MODE(int value) {
+      this.val = value;
+    }
+
+    public int getCodeValue() {
+      return val;
+    }
+
     private int val;
   };
 
@@ -124,8 +137,14 @@ public class LimeLight {
     VISION(0), // Use limelight for CV
     DRIVER(1); // Use limelight for driving (this is dumb, dont do this)
 
-    CAM_MODE(int value) { this.val = value; }
-    public int getCodeValue() { return val; }
+    CAM_MODE(int value) {
+      this.val = value;
+    }
+
+    public int getCodeValue() {
+      return val;
+    }
+
     private int val;
   };
 
@@ -136,7 +155,7 @@ public class LimeLight {
   // PIPELINE
   public static void setPipeline(int pipeline) {
     // Prevent input of invalid pipelines
-    if(pipeline >= 0 && pipeline <= 9) { 
+    if (pipeline >= 0 && pipeline <= 9) {
       table.setNumber("pipeline", pipeline);
     }
   }
@@ -145,8 +164,14 @@ public class LimeLight {
   public enum STREAM { // PIP = Picture-In-Picture
     STANDARD(0), PIP_MAIN(1), PIP_SECONDARY(2);
 
-    STREAM(int value) { this.val = value; }
-    public int getCodeValue() { return val; }
+    STREAM(int value) {
+      this.val = value;
+    }
+
+    public int getCodeValue() {
+      return val;
+    }
+
     private int val;
   };
 
@@ -158,12 +183,18 @@ public class LimeLight {
   public enum SNAPSHOT_MODE {
     STOP(0), // Don't take snapshots
     TAKE_TWO_PER_SECOND(1); // Take two snapshots per second
-    
-    SNAPSHOT_MODE(int value) { this.val = value; }
-    public int getCodeValue() { return val; }
+
+    SNAPSHOT_MODE(int value) {
+      this.val = value;
+    }
+
+    public int getCodeValue() {
+      return val;
+    }
+
     private int val;
   };
-  
+
   public static void setSnapshotMode(SNAPSHOT_MODE snapshot) {
     table.setNumber("snapshot", snapshot.getCodeValue());
   }
