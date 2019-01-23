4050d609562ae9aca4b109a08c9eaf0e505cff2e
doAltplusF4now <kcai00@stuy.edu>
2019-01-23 13:35:46 -0500

    add Profiler

--- ProfileGenerator/CVGenerator/src/main/java/frc/util/CVPath.java
+++ ProfileGenerator/CVGenerator/src/main/java/frc/util/CVPath.java
@@ -1,10 +1,13 @@
 package frc.util;
 
 import java.util.Queue;
+
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.drive.Vector2d;
 import frc.robot.Robot;
-
+import frc.util.Profiler.src.Generation.CenterTraj;
+import frc.util.Profiler.src.Generation.SideTraj;
+import frc.util.Profiler.src.Generation.Waypoint;
 public class CVPath extends Command {
     // How many points on MP path
     static final int PATH_RESOLUTION = 100;
@@ -12,14 +15,20 @@ public class CVPath extends Command {
     static final double MAX_VELOCITY = 0;
     static final double MAX_ACCELERATION = 0;
     static final double MAX_JERK = 0;
+    static final double DT = 0.05;
+    static final double WHEEL_BASE = 12;
+    
+    static Queue ProfileQueue;
     // Angle to left and rightmost targets
     double alpha, beta = 0;
     // Distance from left and rightmost targets
     double d1, d2 = 0;
-    double[] MyTrajectory;
-    Vector2d farGoal, nearGoal,goalPoint;
+    Waypoint[] waypoints;
+    Vector2d farGoal, nearGoal, goalPoint;
     Vector2d h1, h2;// Two handle points
     double m_h2;// Slope of line which the 2nd header point lies on.
+
+    private Waypoint[] data;
 
     public CVPath(double d1, double d2, double alpha, double beta) {
         requires(Robot.drivetrain);
@@ -51,7 +60,7 @@ public class CVPath extends Command {
         goalPoint = new Vector2d((farGoal.x+nearGoal.x)/2.0, (farGoal.y+nearGoal.y)/2.0);
     }
 
-    private void updateSlope() {//gets slopf of the line that the second header point lies on
+    private void updateSlope() {// gets slopf of the line that the second header point lies on
         if (farGoal.y == nearGoal.y) {
             farGoal.y += Math.signum(farGoal.y) / (4096.0);
         }
@@ -64,17 +73,51 @@ public class CVPath extends Command {
     private void updateh1() {
         h1.x = (farGoal.x + nearGoal.x) / 4.0;
     }
-    private void updateh2(){
-        h2.x = ((1.0-R)*Math.abs((nearGoal.x-farGoal.x)/(Math.abs(farGoal.x-nearGoal.x)+Math.abs(farGoal.y-nearGoal.y))) + R)*goalPoint.x;
-        h2.y = m_h2*h2.x-(m_h2*goalPoint.x-goalPoint.y);
+
+    private void updateh2() {
+        h2.x = ((1.0 - R) * Math
+                .abs((nearGoal.x - farGoal.x) / (Math.abs(farGoal.x - nearGoal.x) + Math.abs(farGoal.y - nearGoal.y)))
+                + R) * goalPoint.x;
+        h2.y = m_h2 * (h2.x - goalPoint.x + goalPoint.y);
     }
-    public Queue<double[]> getMyTrajectory() {
-        // x,y,heading,position,velocity,acceleration
-        //TODO: use Tahsin's Profiler to do this
-        return null;
+
+    private void getTime(double t){
+        double[] output = new Double[8];
+        fillTable();
+        ProfileQueue.add()
     }
+
+    private Vector2d getXY(double t) {
+        return new Vector2d(
+                (1 - t) * ((1 - t) * ((1 - t) * 0 + t * h1.x) + t * ((1 - t) * h1.x + t * h2.x))
+                        + t * ((1 - t) * ((1 - t) * h1.x + t * h2.x) + t * ((1 - t) * h2.x + t * goalPoint.x)),
+                (1 - t) * ((1 - t) * ((1 - t) * 0 + t * h1.y) + t * ((1 - t) * h1.y + t * h2.y))
+                        + t * ((1 - t) * ((1 - t) * h1.y + t * h2.y) + t * ((1 - t) * h2.y + t * goalPoint.y)));
+    }
+
+    private void setPoints() {
+        waypoints = new Waypoint[2];
+        waypoints[0] = (new Waypoint(0.0,0.0,0.0));
+        waypoints[1] = (new Waypoint(0.0,0.0,Math.atan(-1/m_h2)));
+    }
+    public void generatePoints(){
+        System.out.println("Generating");
+        CenterTraj centerTraj = new CenterTraj(PATH_RESOLUTION, DT,
+                                                   WHEEL_BASE,
+                                                   MAX_VELOCITY,
+                                                   MAX_ACCELERATION,
+                                                   MAX_JERK,
+                                                   waypoints);
+        centerTraj.generate();
+        SideTraj leftTraj = centerTraj.getLeft(); leftTraj.generate();
+        SideTraj rightTraj = centerTraj.getRight(); rightTraj.generate();
+    }
+    /*public Queue<> getMyTrajectory() {
+        
+    }*/
 
     public boolean isFinished() {
         return false;
     }
+
 }
